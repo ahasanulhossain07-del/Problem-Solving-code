28a109865847509875d99e57977d46d4c38e7447
ahasanulhossain07-del <ahasanulhossain07@gmail.com>
2026-09-18 18:51:09 +0600

    Add Contest-2 A.cpp

---
+++ Contest-2/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+    int l_s=s.length();
+    int l_t=t.length();
+    cout<<l_s<<" "<<l_t<<endl;
+    cout<<s<<" "<<t<<endl;
+
+    return 0;
+}
