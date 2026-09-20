1eda972146225dd29d7a9641dbf8363ef4c77057
ahasanulhossain07-del <ahasanulhossain07@gmail.com>
2026-09-20 12:55:29 +0600

    add solution of palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int copyN=n;
+    int revNum = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        revNum = (revNum * 10) + digit;
+        n = n / 10;
+    }
+    if (revNum == copyN)
+        cout << "Palindrome Number" << endl;
+    else
+        cout << "Not Palindrome Number" << endl;
+
+    return 0;
+}
