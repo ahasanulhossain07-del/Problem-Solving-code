0400cf95006ee5a3262b7e11cf836d83d86ef2d8
ahasanulhossain07-del <ahasanulhossain07@gmail.com>
2026-09-20 10:54:16 +0600

    add sieve of eratosthenes solution

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<bool>prime(n+1,true);
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=i+i;j<=n;j+=i)
+            {
+                prime[j]=false;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            cout<<i<<" ";
+        }
+    }
+
+
+    return 0;
+}
