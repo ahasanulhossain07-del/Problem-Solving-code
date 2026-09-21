50dd4ff9a52a70a657545517cbd6db07ad189499
ahasanulhossain07-del <ahasanulhossain07@gmail.com>
2026-09-21 22:00:42 +0600

    add solution of decimal to binary

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int decNum;
+    cin>>decNum;
+    int ans=0;
+    int pow=1;
+    while(decNum>0)
+    {
+        int rem=decNum%2;
+        decNum=decNum/2;
+        ans+=(rem*pow);
+        pow=(pow*10);
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
